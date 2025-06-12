0808d0d7ce60bb84f196752d954e3d05829f86f7
Anthon676 <cruzcruza676@gmail.com>
2025-06-12 00:07:35 -0600

    Conexión de estudiantes a la base de datos
    Se incorporo la lógica de mostrar los estudiantes inscritos en la actividad correspondiente.

--- Administrador/php/Inscripcion.php
+++ Administrador/php/Inscripcion.php
@@ -10,14 +10,12 @@ class Inscripcion {
     }
 
     public function obtenerAlumnosPorActividad($id_actividad) {
-        $sql = "SELECT u.nombre, u.correo, a.numero_control, a.carrera, a.semestre
+        $sql = "SELECT a.nombre, a.numero_control, a.carrera, a.semestre
                 FROM inscripciones i
                 JOIN alumnos a ON i.id_alumno = a.id_alumno
-                JOIN usuarios u ON a.id_usuario = u.id_usuario
-                WHERE i.id_actividad = :id_actividad";
+                WHERE i.id_actividad = ?";
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(":id_actividad", $id_actividad, PDO::PARAM_INT);
-        $stmt->execute();
+        $stmt->execute([$id_actividad]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
