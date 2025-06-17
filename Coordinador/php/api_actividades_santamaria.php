ce499a79bc0a33df0fd6beade8a41690c3892dab
Anthon676 <cruzcruza676@gmail.com>
2025-06-17 07:29:13 -0600

    Solución de dos modulos
    Se modifico los dos módulos para cargar las actividades

--- Coordinador/php/api_actividades_santamaria.php
+++ Coordinador/php/api_actividades_santamaria.php
@@ -12,11 +12,9 @@ try {
     $id_unidad = 3;
 
     // Ruta base para las imágenes - ajusta según tu estructura de directorios
-<<<<<<< HEAD
-    $baseUrl = "http://localhost/extraescolar/Administrador/";
-=======
+
     $baseUrl = "http://localhost/Valle_Etla/Administrador/";
->>>>>>> aa1cae0dd7cf9d6fcbba7e083f16c451a6b5e686
+
 
     // Solo selecciona las categorías que tengan actividades en esta unidad
     $stmt = $conn->query("SELECT id_categoria, nombre_categoria FROM categorias");
